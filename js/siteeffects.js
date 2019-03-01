42f04d0779747fa8149402275a323b2c60960af0
lemkeprzemyslaw <lemkeprzemyslaw@gmail.com>
2019-03-01 10:48:36 +0100

    add effect to head-container

--- js/siteeffects.js
+++ js/siteeffects.js
@@ -2,15 +2,3 @@ const hederText = $( ".introduction" );
 
 hederText.fadeIn( 4000 );
 
-$(document).ready(function() {
-    var movementStrength = 25;
-    var height = movementStrength / $(window).height();
-    var width = movementStrength / $(window).width();
-    $("#head-container").mousemove(function(e){
-        var pageX = e.pageX - ($(window).width() / 2);
-        var pageY = e.pageY - ($(window).height() / 2);
-        var newvalueX = width * pageX * -1 - 25;
-        var newvalueY = height * pageY * -1 - 50;
-        $('#head-container').css("background-position", newvalueX+"px     "+newvalueY+"px");
-    });
-});
